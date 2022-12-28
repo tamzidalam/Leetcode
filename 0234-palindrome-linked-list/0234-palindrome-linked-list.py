746832d4a97a0aff757bd8d074e6c3974107a99e
tamzidalam <48943522+tamzidalam@users.noreply.github.com>
2022-12-28 02:04:30 -0600

    Time: 821 ms (87.03%), Space: 39.1 MB (79.00%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,50 +9,50 @@ class Solution:
         
         
         
-        fast = slow = head
-        # find the mid node
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        # reverse the second half
-        node = None
-        while slow:
-            nxt = slow.next
-            slow.next = node
-            node = slow
-            slow = nxt
-        # compare the first and second half nodes
-        while node: # while node and head:
-            if node.val != head.val:
-                return False
-            node = node.next
-            head = head.next
-        return True
-        
-#         slow=head
-#         fast=head
-        
+#         fast = slow = head
+#         # find the mid node
 #         while fast and fast.next:
-            
-#             slow=slow.next
-#             fast=fast.next.next
-        
-        
-#         prev=None
-#         rev=slow
-#         while rev:
-#             temp=rev.next
-#             rev.next=prev
-#             prev=rev
-#             rev=temp
-            
-#         left,right=head,prev
-        
-#         while right:
-#             if left.val != right.val:
+#             fast = fast.next.next
+#             slow = slow.next
+#         # reverse the second half
+#         node = None
+#         while slow:
+#             nxt = slow.next
+#             slow.next = node
+#             node = slow
+#             slow = nxt
+#         # compare the first and second half nodes
+#         while node: # while node and head:
+#             if node.val != head.val:
 #                 return False
-#             right=right.next
-#             left=left.next
+#             node = node.next
+#             head = head.next
 #         return True
+        
+        slow=head
+        fast=head
+        
+        while fast and fast.next:
+            
+            slow=slow.next
+            fast=fast.next.next
+        
+        
+        prev=None
+        rev=slow
+        while rev:
+            temp=rev.next
+            rev.next=prev
+            prev=rev
+            rev=temp
+            
+        left,right=head,prev
+        
+        while right:
+            if head.val != right.val:
+                return False
+            right=right.next
+            head=head.next
+        return True
         
        
