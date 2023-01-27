5b4585bb3675a407fe4ecc1a96f529da0e570687
Tamzid Alam <48943522+tamzidalam@users.noreply.github.com>
2023-01-27 13:05:55 -0600

    Time: 3534 ms (51.91%), Space: 13.8 MB (78.54%) - LeetHub

---
+++ 1088-confusing-number-ii/1088-confusing-number-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def confusingNumberII(self, n: int) -> int:
+        rotatables = [1, 0, 6, 8, 9]
+        rotation_map = {1:1, 0:0, 6:9, 8:8, 9:6}        
+        confusing_number_count = 0
+        
+        def count_rotatables(cur_num=0):
+            nonlocal confusing_number_count
+            if cur_num * 10 > n:
+                return
+            for num in rotatables:
+                next_num = cur_num*10 + num
+                if next_num == 0:
+                    continue
+                confusing_number_count += next_num <= n and is_valid_rotation(next_num)
+                count_rotatables(next_num)
+        
+        def is_valid_rotation(num):
+            rotated = 0
+            cur_num = num
+            while cur_num:
+                rotated = rotated * 10 + rotation_map[cur_num%10]
+                cur_num //= 10
+            return rotated != num
+        
+        count_rotatables()
+        
+        return confusing_number_count
+        
