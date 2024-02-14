18a71642357099a5cc6dfdd91a7a16fccfe6fb43
Tamzid Alam <48943522+tamzidalam@users.noreply.github.com>
2024-02-14 17:36:59 -0600

    Time: 39 ms (71.67%), Space: 16.5 MB (98.35%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,26 +1,27 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         
+        
+      
         mapp={}
         
-        
         for i in arr:
-            
             if i in mapp:
-                mapp[i] +=1
-             
+                mapp[i]+=1
+            
             else:
                 mapp[i]=1
+            
         
-        print(mapp)
-       
         sett=set()
         
         for i in mapp.values():
-            sett.add(i)
             
-        if len(mapp) == len(sett):
-            return True
-        else:
-            return False
-                
+            if i in sett:
+                return False
+            else:
+                sett.add(i)
+        
+        
+        return True 
+    
